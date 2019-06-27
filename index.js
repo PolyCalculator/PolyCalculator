a8c1eac83b9e4140ff7acc71de93f005f442c94a
JD Veer <veerjd@gmail.com>
2019-06-27 02:05:40 -0400

    .

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 require('dotenv').config();
 const { Client, RichEmbed } = require('discord.js');
 const bot = new Client();
-const http = require("http")
-
-setInterval(function() {
-    http.get("http://polycalculator.herokuapp.com");
-}, 300000); // every 5 minutes (300000)
+const http = require("http");
 
 const express = require('express');
 var app = express();
@@ -336,6 +332,10 @@ bot.on('message', message => {
 //              END/OTHER
 //--------------------------------------
 const port = process.env.PORT || 5000;
+
+setInterval(function() {
+    http.get("http://polycalculator.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
 
 /*app.get('/', function (req, res) {
     res.send('Hello World!')
