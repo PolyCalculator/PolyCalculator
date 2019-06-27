cbd069cad86255eac16bae715d3ccef8f29f2fc3
JD Veer <veerjd@gmail.com>
2019-06-27 02:16:52 -0400

    .

--- index.js
+++ index.js
@@ -336,13 +336,13 @@ bot.on('message', message => {
 //--------------------------------------
 const port = process.env.PORT || 5000;
 
-/*setInterval(function() {
+setInterval(function() {
     http.get("http://polycalculator.herokuapp.com");
-}, 300000); // every 5 minutes (300000)*/
+}, 300000); // every 5 minutes (300000)
 
-/*app.get('/', function (req, res) {
+app.get('/', function (req, res) {
     res.send('Hello World!')
-})*/
+})
 
 app.listen(port, () => {
     console.log('Listening on ' + port);
