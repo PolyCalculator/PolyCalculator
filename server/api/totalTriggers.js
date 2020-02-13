74adeb1de7e6cf850ee21e3c2aac60b5d6f65874
JD Veer <veerjd@gmail.com>
2020-02-12 23:08:46 -0500

    api+vue kinda almost working!

---
+++ server/api/totalTriggers.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const db = require('../../db')
+
+const router = express.Router();
+
+// Get stats
+router.get('/totalTriggers', async (req, res) => {
+    const { rows } = await db.query('SELECT COUNT(id) AS recordedcount FROM stats')
+    rows[0].recordedcount = Number(rows[0].recordedcount)
+    rows[0].totalCount = rows[0].recordedcount + 6555;
+    res.send(rows)
+})
+
+router.get('/totalTriggers/:guildid', async (req, res) => {
+    const { guildid } = req.params
+    const { rows } = await db.query('SELECT COUNT(id) AS recordedcount FROM stats WHERE server_id = $1', [guildid])
+    rows[0].recordedcount = Number(rows[0].recordedcount)
+    res.send(rows)
+})
+
+module.exports = router
