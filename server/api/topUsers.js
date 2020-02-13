df5ff2eb985327275fab95904950820702e5cfad
JD Veer <veerjd@gmail.com>
2020-02-12 23:08:46 -0500

    api+vue kinda almost working!

---
+++ server/api/topUsers.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const db = require('../../db')
+
+const router = express.Router();
+
+// Get stats
+router.get('/topusers', async (req, res) => {
+    const { rows } = await db.query('SELECT COUNT(id), author_tag FROM stats GROUP BY author_tag ORDER BY COUNT(id) DESC LIMIT 3')
+    res.send(rows)
+})
+
+router.get('/topusers/:limit', async (req, res) => {
+    const { limit } = req.params
+    const { rows } = await db.query('SELECT COUNT(id), author_tag FROM stats GROUP BY author_tag ORDER BY COUNT(id) DESC LIMIT $1', [limit])
+    res.send(rows)
+})
+
+module.exports = router
--- server/index.js
+++ server/index.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const topUsers = require('./api/topUsers')
+const totalTriggers = require('./api/totalTriggers') 
 
 const app = express();
+const stats = express();
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
-const stats = require('./routes/api/stats')
-
 app.use('/api/stats', stats)
+
+// app.get('/', (req, res) => {
+//     res.send(`If you're looking for the API, try http://${req.hostname}/api/docs`)
+// })
+
+app.get('/api', (req, res) => {
+    res.send(`For some docs, try http://${req.hostname}/api/docs`)
+})
+
+// Routers
+    // api/topusers/(limit) for top users
+stats.use('/', topUsers)
+
+    // api/topusers/(guild_id) for top guilds
+stats.use('/', totalTriggers)
 
 const port = process.env.PORT || 3333
 
-app.listen(port, () => {return console.log(`Listening on ${port}`)})
+app.listen(port, () => {return console.log(`Listening on http://localhost:${port}/api`)})
