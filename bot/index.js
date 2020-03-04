79e00fe642818ba392e5084219a5071cd42240fc
JD Veer <veerjd@gmail.com>
2020-03-04 03:46:40 -0500

    Fix missing id for automessages

--- bot/index.js
+++ bot/index.js
@@ -1017,16 +1017,16 @@ setInterval(function() {
     polytopia = bot.guilds.get('283436219780825088')
 
     botcommands = polytopia.channels.get('403724174532673536')
-    botcommands = {'channel':botcommands}
+    botcommands = {'guild':{'id': message.guild.id},'channel':botcommands}
     Help('c', botcommands, true)
     rankedelogames = polytopia.channels.get('511316081160355852')
-    rankedelogames = {'channel':rankedelogames}
+    rankedelogames = {'guild':{'id': message.guild.id},'channel':rankedelogames}
     Help('c', rankedelogames, true)
     unrankedgames = polytopia.channels.get('511906353476927498')
-    unrankedgames = {'channel':unrankedgames}
+    unrankedgames = {'guild':{'id': message.guild.id},'channel':unrankedgames}
     Help('c', unrankedgames, true)
     elobotcommands = polytopia.channels.get('635091071717867521')
-    elobotcommands = {'channel':elobotcommands}
+    elobotcommands = {'guild':{'id': message.guild.id},'channel':elobotcommands}
     Help('c', elobotcommands, true)
 }, 21600000); // every 3h (10800000) 6h (21600000)
 
