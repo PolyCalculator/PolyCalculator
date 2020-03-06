f021d18009dad15a1ad5eba4cfcc1bba348045fe
JD Veer <veerjd@gmail.com>
2020-03-06 11:14:18 -0500

    HOTFIX: too many INSERT

--- bot/stats.js
+++ bot/stats.js
@@ -47,7 +47,7 @@ module.exports.addStats = function (cleanContent, author, cmd, url, resEmbed, gu
     let timeStamp = Date.now();
 
     return new Promise((resolve, reject) => {
-        let sql = `INSERT INTO stats (content, author_id, author_tag, command, url, attacker, defender, date, server_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`
+        let sql = `INSERT INTO stats (content, author_id, author_tag, command, url, attacker, defender, date, server_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
         let values = [cleanContent, author.id, author.tag, cmd.substring(0, 1), url, attacker, defender, timeStamp, guildId]
 
         pool.query(sql, values, (err, res) => {
