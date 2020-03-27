09384f857c596d649cfc7fdf7a53316d53300468
JD Veer <veerjd@gmail.com>
2020-03-27 00:08:06 -0400

    Hid docs cause not ready

--- bot/commands/links.js
+++ bot/commands/links.js
@@ -18,7 +18,7 @@ module.exports = {
       .setTitle('Links!')
       .addField('Invite to my server:', 'https://discordapp.com/oauth2/authorize?client_id=593507058905645057&permissions=8&scope=bot')
       .addField('PolyCalculator\'s server link:', 'https://discord.gg/rtSTmd8')
-      .addField('Documentation (How-to use the bot):', 'https://docs.polycalculatorbot.com')
+      // .addField('Documentation (How-to use the bot):', 'https://docs.polycalculatorbot.com')
       .addField('Website (Stats):', 'https://polycalculatorbot.com')
     this.addStats(message, this.name, willDelete)
       .then().catch(err => { throw err })
