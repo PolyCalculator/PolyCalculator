9c39dcbc4b7435ada2553805c0908acfddd47559
JD Veer <veerjd@gmail.com>
2020-06-08 06:15:43 -0400

    another Easter Egg

---
+++ bot/commands/goochie.js
@@ -0,0 +1,21 @@
+const whatIsGooch = ['He pretty chill', 'He my homeboy', 'He my fav Polytopia Moderator', 'He my UKanian', 'He englishman', 'He da man']
+
+module.exports = {
+  name: 'Goochie',
+  description: 'show fax.',
+  aliases: ['goochie', 'gooch'],
+  shortUsage() {
+    return undefined
+  },
+  longUsage() {
+    return undefined
+  },
+  forceNoDelete: false,
+  category: 'hidden',
+  permsAllowed: ['VIEW_CHANNEL'],
+  usersAllowed: ['217385992837922819'],
+  // eslint-disable-next-line no-unused-vars
+  execute: function(message, argsStr, embed, trashEmoji, data) {
+    return whatIsGooch[Math.floor(Math.random() * whatIsGooch.length)]
+  },
+};
