5d61459b8c721f0da4d2cad7fdaf8d28528ee0bc
JD Veer <veerjd@gmail.com>
2020-10-25 20:51:27 -0400

    Premium command

---
+++ bot/commands/premium.js
@@ -0,0 +1,44 @@
+const db = require('../../db')
+
+module.exports = {
+  name: 'premium',
+  description: 'give premium to someone.',
+  aliases: ['p'],
+  shortUsage(prefix) {
+    return `${prefix}p 217385992837922819 10 notes`
+  },
+  longUsage(prefix) {
+    return `${prefix}premium 217385992837922819 10 notes`
+  },
+  forceNoDelete: true,
+  category: 'hidden',
+  permsAllowed: ['MANAGE_GUILD', 'ADMINISTRATOR'],
+  usersAllowed: ['217385992837922819'],
+  channelsAllowed: ['595323493558517780'],
+  // eslint-disable-next-line no-unused-vars
+  execute: function(message, argsStr, embed, trashEmoji, data) {
+    if(!argsStr)
+      throw 'You need to include information...'
+
+    const args = argsStr.split(/ +/).filter(x => x != '')
+    const userId = args.shift()
+
+    if(userId.length < 18)
+      throw 'Looks like an invalid ID...'
+
+    const amount = Number(args.shift())
+
+    if(isNaN(amount))
+      throw 'Looks like an invalid amount...'
+
+    const notes = args.join(' ') || ''
+
+    const sql = 'INSERT INTO premium (user_id, amount, notes) VALUES ($1, $2, $3)'
+    const values = [userId, amount, notes]
+    db.query(sql, values)
+      .then(() => {
+        message.channel.send('Successfully made premium!')
+      })
+      .catch(err => { throw err })
+  }
+};
