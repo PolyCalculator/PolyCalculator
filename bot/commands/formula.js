fd3120abff9b18c512bfe52faa9426745a5958cf
JD Veer <veerjd@gmail.com>
2021-01-07 20:06:07 -0500

    New formula command

---
+++ bot/commands/formula.js
@@ -0,0 +1,35 @@
+module.exports = {
+  name: 'formula',
+  description: 'show the formula for calculating hp results.',
+  aliases: ['f'],
+  shortUsage(prefix) {
+    return `${prefix}f`
+  },
+  longUsage(prefix) {
+    return `${prefix}formula`
+  },
+  forceNoDelete: true,
+  category: 'Other',
+  permsAllowed: ['VIEW_CHANNEL'],
+  usersAllowed: ['217385992837922819'],
+  execute: function (message, argsStr, replyData) {
+    replyData.title = 'Formula!'
+    replyData.description = 'Last block here is an arithmetic version.\nThe first blocks is in text format.'
+
+    replyData.fields.push({ name: 'Calculate the attacker\'s **force**', value: 'Multiply the attacker\' attack stat with its current HP and divide it by its max HP' })
+    replyData.fields.push({ name: 'Calculate the defender\'s **force**', value: 'Multiply the defender\'s defense stat with its current HP, divide it by its max HP and multiply it by the defense bonus (1, 1.5 or 4)' })
+    replyData.fields.push({ name: 'Calculate the total damage', value: 'Add the attacker\'s force with the defender\'s force' })
+    replyData.fields.push({ name: 'The defender\'s HP lost', value: 'Divide the attacker\'s force by the total damage, timed by the attacker\'s attack stat and by 4.5\nResult rounded up' })
+    replyData.fields.push({ name: 'The attacker\'s HP lost', value: 'Divide the defender\'s force by the total damage, timed by the defender\'s attack stat and by 4.5\nResult rounded down' })
+
+    const shortFormula = []
+    shortFormula.push('\nattacker.force = attacker.attackStats x attacker.currentHP / attacker.maxHP\n')
+    shortFormula.push('defender.force = defender.defenderStats x solution.defenderHP / defender.maxhp x defender.bonus\n')
+    shortFormula.push('totalDamage = attacker.force + defender.force\n')
+    shortFormula.push('defender.HPlost = attacker.force / totalDamage x attacker.attackStats x 4.5 rounded up\n')
+    shortFormula.push('attacker.HPlost = defender.force / totalDamage x defender.defenderStats x 4.5 round down\n')
+    replyData.fields.push({ name: 'Arithmetic format:', value: shortFormula })
+
+    return replyData
+  }
+};
