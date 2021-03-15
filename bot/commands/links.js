f5390f942601e966c961fe2afea033745ef7107d
JD Veer <veerjd@gmail.com>
2021-03-15 13:02:59 -0400

    Updated links

--- bot/commands/links.js
+++ bot/commands/links.js
@@ -16,8 +16,8 @@ module.exports = {
     replyData.discord.title = 'Links!'
     replyData.discord.fields.push({ name: 'Invite this bot to your server:', value: 'https://discord.com/oauth2/authorize?client_id=593507058905645057&scope=bot&permissions=93256' })
     replyData.discord.fields.push({ name: 'PolyCalculator\'s server link:', value: 'https://discord.gg/rtSTmd8' })
-    replyData.discord.fields.push({ name: 'Documentation (How-to use the bot):', value: 'https://alphaSeahorse.gitbook.io/polycalculator' })
-    replyData.discord.fields.push({ name: 'Website (Stats):', value: 'http://polycalculatorbot.com' })
+    replyData.discord.fields.push({ name: 'Documentation (How-to use the bot):', value: 'https://docs.polycalculatorbot.com/' })
+    replyData.discord.fields.push({ name: 'Web bot (only `.calc`):', value: 'https://cli.polycalculatorbot.com' })
 
     return replyData
   }
