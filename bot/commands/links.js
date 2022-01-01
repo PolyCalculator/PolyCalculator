0d53a62de0f03947ff0f27b06cb3baaac943c457
JD Veer <veerjd@gmail.com>
2022-01-01 06:38:34 -0500

    Modify invite link to include slash command perm

--- bot/commands/links.js
+++ bot/commands/links.js
@@ -13,7 +13,7 @@ module.exports = {
   usersAllowed: ['217385992837922819'],
   execute: function(message, argsStr, replyData/*, dbData*/) {
     replyData.discord.title = 'Links!'
-    replyData.discord.fields.push({ name: 'Invite this bot to your server:', value: 'https://discord.com/oauth2/authorize?client_id=593507058905645057&scope=bot&permissions=93256' })
+    replyData.discord.fields.push({ name: 'Invite this bot to your server:', value: 'https://discord.com/oauth2/authorize?client_id=593507058905645057&scope=bot&permissions=2147576904' })
     replyData.discord.fields.push({ name: 'PolyCalculator\'s server link:', value: 'https://discord.gg/rtSTmd8' })
     replyData.discord.fields.push({ name: 'Documentation (How-to use the bot):', value: 'https://docs.polycalculatorbot.com/' })
     replyData.discord.fields.push({ name: 'Web bot (only `.calc`):', value: 'https://cli.polycalculatorbot.com' })
