bf116ca3774aecd35ef2c8e9b47567437414ff2f
JD Veer <veerjd@gmail.com>
2022-05-04 21:54:40 -0400

    Change bad links

--- bot/commands/credits.js
+++ bot/commands/credits.js
@@ -13,7 +13,7 @@ module.exports = {
   usersAllowed: ['217385992837922819'],
   execute: function(message, argsStr, replyData/*, dbData*/) {
     replyData.discord.title = '**PolyCalculator bot credits!**'
-    replyData.discord.fields.push({ name: 'Developer', value: 'jd (alphaSeahorse)' })
+    replyData.discord.fields.push({ name: 'Developer', value: 'jd (akajd)' })
     replyData.discord.fields.push({ name: 'Contributions', value: 'penile partay, MYRIAD CARDS, espark, Shiny, LiNoKami, HelloIAmBush, Cake, James.' })
 
     return replyData
--- bot/commands/links.js
+++ bot/commands/links.js
@@ -16,7 +16,7 @@ module.exports = {
     replyData.discord.fields.push({ name: 'Invite this bot to your server:', value: 'https://discord.com/oauth2/authorize?client_id=593507058905645057&scope=bot%20applications.commands&permissions=2147576904' })
     replyData.discord.fields.push({ name: 'PolyCalculator\'s server link:', value: 'https://discord.gg/rtSTmd8' })
     replyData.discord.fields.push({ name: 'Documentation (How-to use the bot):', value: 'https://docs.polycalculatorbot.com/' })
-    replyData.discord.fields.push({ name: 'Web bot (only `.calc`):', value: 'https://cli.polycalculatorbot.com' })
+    // replyData.discord.fields.push({ name: 'Web bot (only `.calc`):', value: 'https://cli.polycalculatorbot.com' })
 
     return replyData
   }
