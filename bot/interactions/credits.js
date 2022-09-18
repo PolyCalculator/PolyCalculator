fbc0003bea72c0e292e82b394f76299a1a6bb6a4
JD Veer <veerjd@gmail.com>
2022-09-17 20:39:50 -0400

    Added /credits

---
+++ bot/interactions/credits.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const credits = require('../commands/credits')
+
+module.exports = {
+  dev: false,
+  data: new SlashCommandBuilder()
+    .setName('credits')
+    .setDescription('PolyCalculator bot credits!'),
+  async execute(interaction, replyData, dbData) {
+    dbData.content = `${interaction.commandName}`
+
+    return await credits.execute({}, '', replyData, dbData)
+  },
+};
+
