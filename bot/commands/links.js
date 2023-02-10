3d1bcf6a7c79f1bc6b7b813dec68bc4c299f5fa5
JD Veer <veerjd@gmail.com>
2023-02-10 10:10:49 -0500

    Added `links` command

--- bot/commands/links.js
+++ bot/commands/links.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'links',
-  description: 'show the link to invite the bot, to the developer\'s server and to the statistic\'s website.',
+  description: 'show the link to invite the bot and to the developer\'s server.',
   aliases: ['link', 'inv', 'invite', 'server'],
   shortUsage(prefix) {
     return `${prefix}inv`
@@ -15,7 +15,7 @@ module.exports = {
     replyData.discord.title = 'Links!'
     replyData.discord.fields.push({ name: 'Invite this bot to your server:', value: 'https://discord.com/oauth2/authorize?client_id=593507058905645057&scope=bot%20applications.commands&permissions=2147576904' })
     replyData.discord.fields.push({ name: 'PolyCalculator\'s server link:', value: 'https://discord.gg/rtSTmd8' })
-    replyData.discord.fields.push({ name: 'Documentation (How-to use the bot):', value: 'https://docs.polycalculatorbot.com/' })
+    // replyData.discord.fields.push({ name: 'Documentation (How-to use the bot):', value: 'https://docs.polycalculatorbot.com/' })
     // replyData.discord.fields.push({ name: 'Web bot (only `.calc`):', value: 'https://cli.polycalculatorbot.com' })
 
     return replyData
---
+++ bot/interactions/links.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const links = require('../commands/links')
+
+module.exports = {
+  dev: false,
+  data: new SlashCommandBuilder()
+    .setName('links')
+    .setDescription('Show the links to invite the bot and to the developer\'s server!'),
+  async execute(interaction, replyData, dbData) {
+    dbData.content = `${interaction.commandName}`
+
+    return await links.execute({}, '', replyData, dbData)
+  },
+};
+
