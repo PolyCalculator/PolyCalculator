e2da91e04b8e3224258ee2c5ddf4af3602e73cec
Ivaylo Bratoev <ivo@st6.io>
2023-12-14 17:45:14 +0200

    Add tests to cover all scenarios between wa and de
    Cover all possible fight scenarios with WA and DE.
    My end goal with the simpleCalc tests is to cover all scenarios between units with different attack and defense.
    
    You can read more about snapshot testing with Jest here: https://jestjs.io/docs/snapshot-testing
    Snapshot testing is a good way to detect changes to calculator results when the code is changed.

---
+++ bot/tests/simpleCalc/de-vs-wa.test.js
@@ -0,0 +1,28 @@
+const { test, expect } = require('@jest/globals');
+const { execute } = require('../../commands/calc.js');
+const { generateTestSuit, replyData } = require('./utils.js');
+
+const testData = () =>
+  generateTestSuit('de', ['b', 'v', 'b v'], 'wa', [
+    'd',
+    'd v',
+    'p',
+    'p v',
+    'w',
+    'w v',
+    'v',
+  ]);
+
+testData().forEach((cmd) => {
+  test(cmd, () => {
+    const reply = replyData();
+    execute({}, cmd, reply, {});
+    const result = {
+      _cmd: cmd, // use underscore to put it on top of the snapshot
+      attacker: reply.outcome.attackers[0].afterhp,
+      defender: reply.outcome.defender.afterhp,
+    };
+
+    expect(result).toMatchSnapshot();
+  });
+});
---
+++ bot/tests/simpleCalc/utils.js
@@ -0,0 +1,65 @@
+const { getUnit } = require('../../commands/units.js');
+
+const getTestUnit = (code, modifier) => {
+  const unit = getUnit(code);
+  unit.code = code;
+  if (modifier) {
+    unit.modifiers = ' ' + modifier;
+  } else {
+    unit.modifiers = '';
+  }
+  return unit;
+};
+
+const getMaxHp = (unit) => {
+  if (unit.modifiers.includes('v')) {
+    return unit.maxhp + 5;
+  }
+  return unit.maxhp;
+};
+
+const generateTests = (attacker, defender) => {
+  const tests = [];
+  for (let a = 1; a <= getMaxHp(attacker); a++) {
+    for (let d = 1; d <= getMaxHp(defender); d++) {
+      tests.push(
+        `${attacker.code}${attacker.modifiers} ${a}, ${defender.code}${defender.modifiers} ${d}`,
+      );
+    }
+  }
+  return tests;
+};
+
+const generateTestSuit = (
+  attCode,
+  attModifierList,
+  defCode,
+  defModifierList,
+) => {
+  const result = [];
+  attModifierList.forEach((attModifier) => {
+    defModifierList.forEach((defModifier) => {
+      const att = getTestUnit(attCode, attModifier);
+      const def = getTestUnit(defCode, defModifier);
+      result.push(...generateTests(att, def));
+    });
+  });
+  return result;
+};
+
+const replyData = () => ({
+  content: [],
+  deleteContent: false,
+  discord: {
+    title: undefined,
+    description: undefined,
+    fields: [],
+    footer: undefined,
+  },
+  outcome: {
+    attackers: [],
+    defender: {},
+  },
+});
+
+module.exports = { generateTests, getTestUnit, generateTestSuit, replyData };
