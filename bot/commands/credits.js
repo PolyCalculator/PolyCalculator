6292485a2f6684069d88ba49ad3bddc88964f361
JD Veer <veerjd@gmail.com>
2023-12-16 18:56:48 -0500

    Added ibra to credits

--- bot/commands/credits.js
+++ bot/commands/credits.js
@@ -13,7 +13,8 @@ module.exports = {
   usersAllowed: ['217385992837922819'],
   execute: function(message, argsStr, replyData/*, dbData*/) {
     replyData.discord.title = '**PolyCalculator bot credits!**'
-    replyData.discord.fields.push({ name: 'Developer', value: 'jd (akajd)' })
+    replyData.discord.fields.push({ name: 'Lead Developer', value: 'jd (akajd)' })
+    replyData.discord.fields.push({ name: 'Development team', value: 'ibra9' })
     replyData.discord.fields.push({ name: 'Contributions', value: 'penile partay, MYRIAD CARDS, espark, Shiny, LiNoKami, HelloIAmBush, Cake, James.' })
 
     return replyData
