86ffec44c52b3e71817aa1746fcfe4a0e5645a17
JD Veer <veerjd@gmail.com>
2023-12-17 08:03:17 -0500

    Added clarity and flexibility for deploying beta/preprod commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -37,13 +37,18 @@ if (process.env.NODE_ENV === 'prod') {
   console.log('[DEV] Started refreshing application (/) commands.');
   const rest = new REST({ version: '9' }).setToken(TOKEN);
   (async () => {
+    // const body = commands
+    const body = []
     try {
       await rest.put(
         Routes.applicationGuildCommands(CLIENTID, GUILDID),
-        { body: commands },
+        { body: body },
       );
 
-      console.log('Successfully reloaded application (/) commands.');
+      if(body == commands)
+        console.log("\x1b[42m",'Successfully reloaded application (/) commands.');
+      else
+        console.log("\x1b[41m",'Removing application (/) commands.');
     } catch (error) {
       console.error(error)
       console.error(error.rawError.errors[0]);
